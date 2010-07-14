4c14e66b771d49e7a37da04652c895e60310be15
Andrew Latimer <andrew@elpasoera.com>
2010-07-13 18:27:39 -0600

    restarting

--- clone_and_link.sh
+++
@@ -1,9 +0,0 @@
-cd &&
-[ -d '.dot-files' ] || git clone git://github.com/ahlatimer/dot-files.git .dot-files &&
-ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
-  [ "$f" == '.dot-files/files/.' ] ||
-  [ "$f" == '.dot-files/files/..' ] ||
-  [ "$f" == '.dot-files/files/.git' ] ||
-  ln -vsf "$f" .
-done
-
